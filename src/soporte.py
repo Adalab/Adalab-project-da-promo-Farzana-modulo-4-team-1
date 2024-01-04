fdbd80cfc0300cf9591844788c028ad9239dca5d
Tamara Garcia <tamara.garmart@gmail.com>
2024-01-04 11:44:30 +0100

    actualizaciión limpieza

--- src/soporte.py
+++ src/soporte.py
@@ -15,15 +15,19 @@ df.head(10)
 #%%
 df[['MaritalDesc', 'MaritalStatusID','MarriedID']]
 # %%
-columnas_eliminar = ['MarriedID','Sex']
+columnas_eliminar = ['MarriedID', 'Sex', 'Date_of_Birth', 'Age', 'date', 'PositionID', 'MaritalStatusID']
+
+df.drop(columnas_eliminar, axis=1, inplace=True)
+
 #Employee_Name: separar apellido y nombre
 #Gender: 0 Femenino, 1= Masculino
-#EmpStatusID: 1, 2, 3 = Active
-# 4 = Terminated for Cause
-# 5= Voluntarily Terminated
 #TermR: 1. No estan y 0. Estan 
 #EmploymentStatus: Active, Terminated for Cause,Voluntarily Terminated	
 #TermReason: Cambiar a N/A-StillEmployed si el termd es 0 y EmploymentStatus = Active
+
+# %%
+df = df.drop(df.index[311:409])
+
 #%%
 df[['Sex', 'GenderID','Employee_Name'	]].head(20)
 # %%
@@ -42,4 +46,22 @@ df[['TermReason','EmploymentStatus','Termd', 'EmpStatusID']].sample(20)
 # %%
 #Data frame correspondiente para cambiar TermReason a Still Employed
 df[df['Termd'] == 0.0][df['TermReason']!= 'N/A-StillEmployed']
+#%%
+mapa = {2.0 : 1.0}
+df['EmpStatusID'] = df['EmpStatusID'].replace(mapa)
+
+mapa_2 = {3.0 : 1.0}
+df['EmpStatusID'] = df['EmpStatusID'].replace(mapa_2)
+
+mapa_3 = {4.0 : 2.0}
+df['EmpStatusID'] = df['EmpStatusID'].replace(mapa_3)
+
+mapa_4 = {5.0 : 3.0}
+df['EmpStatusID'] = df['EmpStatusID'].replace(mapa_4)
+
 # %%
+# BOD - fecha
+
+df[['DateofHire', 'DateofTermination']] = pd.to_datetime(df[['DateofHire', 'DateofTermination']], format='%m/%d/%Y')
+
+fecha_datetime = datetime.strptime(fecha_string, '%m/%d/%Y')
